8817d6303df56d0bbf585fd2b7db0e6de9793aaa
jacky <jacky@eelf.cn>
2026-01-19 01:11:12 +0800

    增加日志中ILog中的Trace,Task方法;增加放弃任务安全扩展方法ForgetSafeTask;

---
+++ Threading/Extentions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/****************************************************************
+*  Copyright © (2026) www.eelf.cn All Rights Reserved.          *
+*  Author : jacky                                               *
+*  QQ : 7092734                                                 *
+*  Email : jacky@eelf.cn                                        *
+*  Site : www.eelf.cn                                           *
+*  Create Time : 2026-01-19 01:00:11                            *
+*  Version : v 1.0.0                                            *
+*  CLR Version : 4.0.30319.42000                                *
+*****************************************************************/
+namespace XiaoFeng.Threading
+{
+    /// <summary>
+    /// 扩展
+    /// </summary>
+    public static class Extentions
+    {
+        #region 方法
+        /// <summary>
+        /// 放弃任务安全
+        /// </summary>
+        /// <param name="task">任务</param>
+        public static void ForgetTaskSafe(this Task task)
+        {
+            task.ContinueWith(t =>
+            {
+                if (t.Exception != null)
+                {
+                    LogHelper.Error(t.Exception.InnerException, "任务出错:");
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted).ConfigureAwait(false);
+        }
+        #endregion
+    }
+}
